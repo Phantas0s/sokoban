04bf19b6d09c9c3324947be6dbc647b600cc7f8d
matthieu <matthieu.cneude@gmail.com>
2020-06-18 21:28:13 +0200

    Load different level and need to find a way to load dynamically local files...

--- src/sokoban/core.cljc
+++ src/sokoban/core.cljc
@@ -3,6 +3,8 @@
             [sokoban.move :as move]
             [sokoban.collision :as coll]
             [sokoban.tiles :as ti]
+            #?(:cljs [cljs.reader :refer [read-string]]
+               :clj [clojure.core :refer [read-string]])
             [play-cljc.gl.core :as c]
             [play-cljc.gl.entities-2d :as e]
             [play-cljc.transforms :as t]
@@ -13,13 +15,27 @@
             #?(:clj  [sokoban.tiles :as tiles :refer [read-tiled-map]]
                :cljs [sokoban.tiles :as tiles :refer-macros [read-tiled-map]])))
 
+(def level-1 (read-tiled-map "level1.tmx"))
+(def level-2 (read-tiled-map "level2.tmx"))
+; (def level-3 (read-tiled-map "level3.tmx"))
+
 (defonce *state (atom {:pressed-keys #{}
                        :player-pos []
+                       :level 0
+                       :levels (vector level-1 level-2)
                        :player-moves {}
                        :tiled-map {}
                        :player-images {}}))
 
-(def tiled-map (edn/read-string (read-tiled-map "level1.tmx")))
+(defn read-level [level]
+  (edn/read-string level))
+
+(defn load-level [game {:keys [level levels] :as state}]
+  (tiles/load-tiled-map game (read-level (nth levels level))
+                        (fn [tiled-map]
+                          (let [player-start (into {} (filter #(= (% :layer) "player-start") (-> tiled-map :tiles)))]
+                            (swap! *state assoc :tiled-map tiled-map
+                                   :player-pos (:pos player-start))))))
 
 (defn init [game]
   (gl game enable (gl game BLEND))
@@ -35,22 +51,18 @@
                                                         0
                                                         player-width
                                                         player-height) :width player-width :height player-height)))
-                           [image1] images
-                           player-start (into {} (filter #(= (% :layer) "player-start") (-> (:tiled-map @*state) :tiles)))]
+                           [image1] images]
                        (swap! *state assoc
-                              :player-pos (:pos player-start)
                               :player-images {:image1 image1}))))
-  (tiles/load-tiled-map game tiled-map
-                        (fn [tiled-map]
-                          (swap! *state assoc :tiled-map tiled-map))))
+  (load-level game @*state))
 
 (def screen-entity
   {:viewport {:x 0 :y 0 :width 0 :height 0}
    :clear {:color [(/ 255 255) (/ 255 255) (/ 255 255) 1] :depth 1}})
 
-(defn win? [{:keys [tiled-map] :as state}]
+(defn win? [game {:keys [tiled-map level levels] :as state}]
   (if (ti/same-position? tiled-map ["boxes" "goals"])
-    (pprint "WIIINNN")
+    (load-level game state)
     state))
 
 (defn tick [game] game
@@ -78,6 +90,6 @@
     (->> state
          (move/player-move game)
          (coll/player-interactions game)
-         (win?)
-         (reset! *state))
+         (reset! *state)
+         (win? game))
     game))
---
+++ src/sokoban/tiles.cljs
@@ -0,0 +1,128 @@
+(ns sokoban.tiles
+  (:require
+   [sokoban.utils :as utils]
+   [play-cljc.transforms :as t]
+   [play-cljc.instances :as i]
+   [clojure.pprint :refer [pprint]]
+   [cljs.nodejs :as nodejs]
+   [play-cljc.gl.core :as c]
+   [play-cljc.gl.entities-2d :as e]))
+   ; #?@(:clj [[clojure.java.io :as io]
+   ;           [tile-soup.core :as ts]])))
+
+; #?(:clj (defmacro read-tiled-map [fname]
+;           (-> (str "public/" fname)
+;               io/resource
+;               slurp
+;               ts/parse
+;               pr-str)))
+; (#?(:cljs (defn node-slurp [path]
+;             (pprint path)
+;             (let [fs (js/require "fs")]
+;               (.readFileSync fs path "utf8")))))
+
+(defn node-slurp [path]
+  (let [fs (nodejs/require "fs")]
+    (.readFileSync fs path "utf8")))
+
+(def box-goal-id 14)
+
+(defn crop-tileset [tileset width height tilewidth tileheight]
+  "Crop the tileset (must be included in the parsed map) and return each tile as image"
+  (let [tiles-vert (/ height tileheight)
+        tiles-horiz (/ width tilewidth)]
+    (vec
+     ; (map #(t/crop tileset (* %1 tilewidth) (* %2 tileheight) tilewidth tileheight) (vec (range tiles-horiz)) (vec (range tiles-vert)))
+     (for [y (range tiles-vert)
+           x (range tiles-horiz)]
+       (t/crop tileset
+               (* x tilewidth)
+               (* y tileheight)
+               tilewidth
+               tileheight)))))
+
+(defn load-tiled-map [game parsed callback]
+  "Parse a map (tmx) from the tiled software"
+  (let [map-width (-> parsed :attrs :width) ; number of tiles
+        map-height (-> parsed :attrs :height) ; number of tiles
+        tileset (first (filter #(= :tileset (:tag %)) (:content parsed)))
+        image (first (filter #(= :image (:tag %)) (:content tileset)))
+        {{:keys [tilewidth tileheight]} :attrs} tileset
+        layers (->> parsed :content
+                    (filter #(= :layer (:tag %)))
+                    (map #(vector
+                           (-> % :attrs :name)
+                           (-> % :content first :content first)))
+                    (into {}))]
+    (utils/get-image (-> image :attrs :source)
+                     (fn [{:keys [data width height]}]
+                       (let [entity (e/->image-entity game data width height)
+                             tiles-img (crop-tileset entity width height tilewidth tileheight)
+                             {:keys [layers tiles entities]}
+                             (reduce
+                              (fn [m layer-name]
+                                (let [layer (get layers layer-name)]
+                                  (reduce
+                                   (fn [m i]
+                                     (let [x (mod i map-width)
+                                           y (int (/ i map-width))
+                                           image-id (dec (nth layer i))
+                                           tile-map (when (>= image-id 0)
+                                                      {:layer layer-name :pos (vector x y)})]
+                                       (cond-> m
+                                         true (assoc-in [:layers layer-name x y] tile-map)
+                                         tile-map (update :tiles conj tile-map)
+                                         tile-map (update :entities conj (t/translate (nth tiles-img image-id) x y)))))
+                                   m
+                                   (range (count layer)))))
+                              {:layers {}
+                               :tiles []
+                               :entities []}
+                              ["background" "walls" "boxes" "goals" "player-start"])
+                             entity (i/->instanced-entity entity)
+                             entity (c/compile game entity)
+                             entity (reduce-kv i/assoc entity entities)]
+                         (callback
+                          {:layers layers
+                           :tiles tiles
+                           :tile-map-entity entity
+                           :entities entities
+                           :tilesheet tiles-img
+                           :map-width map-width
+                           :map-height map-height}))))))
+
+(defn tile-from-layer
+  [tiled-map layer]
+  (filter #(= (:layer %) layer) (:tiles tiled-map)))
+
+; TODO this SHOULD be decomposed but I'm kind of proud of it :D
+(defn same-position?
+  [tiled-map layers]
+  (let [tiles (flatten (map (fn [layer] (filter (fn [tile] (= (:layer tile) layer)) (:tiles tiled-map))) layers))]
+    (= (/ (count tiles) (count layers)) (count (reduce (fn [v m] (conj v (:pos m))) #{} tiles)))))
+
+; (defn same-position?
+;   [tiled-map pos]
+;   (filter #(= (:pos %) pos) (:tiles tiled-map)))
+
+(defn tile-from-position [tiled-map layer pos]
+  "Get a tile from a layer and a position"
+  (get-in (get (:layers tiled-map) layer) pos))
+
+(defn tile-id [tile-map tile]
+  (.indexOf (:tiles tile-map) tile))
+
+(defn move-tile [tiled-map tile tile-id new-pos [dir-x dir-y] sprite]
+  (let [layer (:layer tile)
+        [old-x old-y] (:pos tile)
+        [new-x new-y] new-pos
+        new-entity (sprite new-pos)
+        tile-moved (assoc tile :pos new-pos)
+        new-entities (into (conj (subvec (:entities tiled-map) 0 tile-id) (t/translate new-entity dir-x dir-y)) (subvec (:entities tiled-map) (inc tile-id)))]
+    (-> tiled-map
+        (assoc-in [:layers layer old-x old-y] nil)
+        (assoc-in [:layers layer new-x new-y] tile-moved)
+        (assoc
+         :tiles (into (conj (subvec (:tiles tiled-map) 0 tile-id) tile-moved) (subvec (:tiles tiled-map) (inc tile-id)))
+         :entities new-entities
+         :tile-map-entity (reduce-kv i/assoc (:tile-map-entity tiled-map) new-entities)))))
